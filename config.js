abc11c4bc844091ba0ba05df35097f4c7ee5e359
Muragijimana <beastar457@gmail.com>
2018-06-22 13:32:04 +0200

    working

--- config.js
+++ config.js
@@ -1,22 +1,34 @@
+var fetch = require('node-fetch');
 module.exports = {
     // Replace this with a container that you own.
-    containerIdentifier:'iCloud.com.commonworldinc.dev.Dice',
-  
-    // containerIdentifier: 'iCloud.com.commonworldinc.dev.Dice',
-    // apiToken: 'd38ab6e66909f5080aaa9f11684e3dc536e615bf8bf7b95b0b495d8a4d1b3c29',
-    // environment: 'production'
-    environment: 'development',
-  
-    apiTokenAuth: {
-        apiToken: '14a06cef976e5a0329150546ee3f4fb6867e8d9b0c99211f01b7775c07615c4c'
-    },
-    // serverToServerKeyAuth: {
-    //   // Generate a key ID through CloudKit Dashboard and insert it here.
-    //   keyID: '14a06cef976e5a0329150546ee3f4fb6867e8d9b0c99211f01b7775c07615c4c',
-  
-    //   // This should reference the private key file that you used to generate the above key ID.
-    //   privateKeyFile: __dirname + '/eckey.pem'
-    // }
+    // containerIdentifier:'iCloud.com.commonworldinc.dev.Dice',
+    // apiTokenAuth: {
+    //     apiToken: '14a06cef976e5a0329150546ee3f4fb6867e8d9b0c99211f01b7775c07615c4c'
+    // },
+    // environment: 'development'
+
+    services: {
+        fetch: fetch,
+        logger: console
+      },
+      containers: [{
+    
+        // Change this to a container identifier that you own.
+        containerIdentifier: 'iCloud.com.commonworldinc.dev.Dice',
+    
+        environment: 'production',
+    
+        serverToServerKeyAuth: {
+    
+          // This is the key ID you generated in CloudKit Dashboard.
+          keyID: '14a06cef976e5a0329150546ee3f4fb6867e8d9b0c99211f01b7775c07615c4c',
+    
+          // This should reference the private key file that you used to
+          // generate the above key ID.
+          privateKeyFile: __dirname + '/eckey.pem'
+    
+        }
+      }]
   };
 
   
--- index.js
+++ index.js
@@ -1,29 +1,38 @@
 // content of index.js
 const http = require('http')
 var CloudKit = require("./cloudkit.js")
-var fetch = require('node-fetch');
+
 var containerConfig = require('./config');
 
 const port = 3000
-CloudKit.configure({
-    // services: {
-    //     fetch: fetch
-    // },
-    containers: [containerConfig]
-});
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+CloudKit.configure(containerConfig);
 var container = CloudKit.getDefaultContainer();
 var publicDB = container.publicCloudDatabase;
 container.setUpAuth;
 function demoPerformQuery() {
     // console.log(publicDB);
-    
-    publicDB.performQuery({recordType: 'Items'}).then(function(response){
-      console.log(response)
-    }).catch(function(error){
-      console.log(error)
-    })
-  }
+    container.setUpAuth()
+        .then(function (userInfo) {
+            // The userInfo is that of the user who created the key in CloudKit Dashboard.
+            // If this user has permission to write a record of type Item to the
+            // public database, the following call will succeed.
+            // return database.saveRecords({ recordType: 'Item' });
+            publicDB.performQuery({ recordType: 'Roll' }).then(function (response) {
+                console.log(response)
+            }).catch(function (error) {
+                console.log(error)
+            })
+
+        }).then(function (response) {
+            var savedRecord = response.records[0];
+            console.log('Record name:', savedRecord.recordName);
+            process.exit(0);
+        }).catch(function (error) {
+            // Handle the error.
+            process.exit(1);
+        });
+
+}
 
 const requestHandler = (request, response) => {
     demoPerformQuery()
